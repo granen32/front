88a687d88ce816690ee45b97103593eb1ffa6ee3
rnjsrbgud@naver.com <rnjsrbgud@naver.com>
2022-05-25 17:57:03 +0900

    투두리스트 리액트 작성

--- granen/src/App.js
+++ granen/src/App.js
@@ -1,10 +1,36 @@
-import Button from "./component/Button";
-import styles from "./assets/css/App.module.css";
+import { useState } from 'react';
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  // 초기값 설정 문자열로 설정
+  const onChange = (event) =>{
+    setTodo(event.target.value);
+    // 상태값을 변경하는 함수를 활용하여 벨류값을 가져옴
+  }
+  const onSubmit = (event) =>{
+    event.preventDefault();
+    if(todo === ""){
+      return
+    }
+    setTodo("");
+    setTodos((currentTodo => [todo, ...currentTodo]));
+
+  }
+  console.log(todos);
   return (
     <>
-      <h1 className={styles.title}>hi react</h1>
-			<Button text="Continue"/>
+    <h1>MY TODOS ({todos.length})</h1>
+    <form action="" onSubmit={onSubmit}>
+      <input 
+        onChange={onChange}
+        value={todo} 
+        type="text" 
+        placeholder="Write your to do..."
+      />
+      <button>submit</button>
+    </form>
+
     </>
   );
 }

1a4712bba924444c99be5752a2b4a33443661745
rnjsrbgud@naver.com <rnjsrbgud@naver.com>
2022-06-04 15:37:53 +0900

    javascript study

---
+++ vanila_test/study/null.js
@@ -0,0 +1,7 @@
+let a;
+console.log(a);
+// 자료형을 아무것도 할당하지 않으면 언디파인드가 나오지만
+// 일부러 그것을 유도를 할 경우에는 null 을 사용해줘야한다
+
+let ab = null;
+console.log(ab);
---
+++ vanila_test/study/number.js
@@ -0,0 +1,10 @@
+let age = 25; 
+// 정수
+let tall = 175.9;
+// 실수
+
+let inf =Infinity;
+// 무한대
+let minusinf = -Infinity;
+let nan = NaN;
+// 수학적 연산 실패시 나타남

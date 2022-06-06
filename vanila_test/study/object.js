bdd9d9e96b497eabf01838f895a26055228d2b84
rnjsrbgud@naver.com <rnjsrbgud@naver.com>
2022-06-06 22:38:07 +0900

    object

--- vanila_test/study/object.js
+++ vanila_test/study/object.js
@@ -10,11 +10,13 @@
   let person2 = {
     key : "value",
     key2 : "value2",
+    // 프로퍼티-> 멤버라함
     key3 : true,
     key4 : undefined,
     key5 : [1,2],
     key6 :function(){
-
+      // 객체의 메서드라함
+      console.log(`안녕 나는 ${person2["key2"]}`);
     }
   };
   console.log(person2);
@@ -28,3 +30,30 @@
   키/벨류를 다 가지는 걸 프로퍼티라 얘기한다. 객체 프로퍼티라함
   즉 객체속성임 프로퍼티 벨류에는 무엇이 와도 상관이 없음
  */
+
+  // 객체 접근법
+  console.log(person2.key2);
+  // 점 표현식으로 프로퍼티 값에 접근할 수 있다 점 표기법이라함
+  console.log(person2["key4"]);
+  // 괄호 표기법 저기 키부분을 문자열 형식으로 꼭 넣어줘야함
+  // 네임을 변수로 인식하기 때문에 꼭 네임 변수를 만들어주든 아니라면 키값을 넣어줘야함
+
+let person3 = {
+  name : '이정환',
+  age :25
+}
+console.log(getPropertyValue("name"));
+function getPropertyValue(key){
+  return person3[key];
+}
+
+person3.location = "한국";
+console.log(person3);
+// 프로퍼티를 추가 및 변경할 때는 점표기법을 활용할 수 있다.
+
+// 삭제하는 법
+delete person3.age;
+
+// 기존 프로퍼티를 널로 바꿔줘도 삭제가된다!
+
+console.log(`name : ${"name" in person2}`);

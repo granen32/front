9be481da4fb4ad1648925b959d71f300961bd4d6
rnjsrbgud@naver.com <rnjsrbgud@naver.com>
2022-07-01 22:21:57 +0900

    coin tracker

--- ts-react/src/component/Coin.tsx
+++ ts-react/src/component/Coin.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
 
 const Coin = () => {
+  const {coinId} = useParams();
+  console.log(coinId);
   return (
     <>
       <h1>conin</h1>

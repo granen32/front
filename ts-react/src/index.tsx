8160aeeea2607bb888da311bdfe8ea05ec448ed0
rnjsrbgud@naver.com <rnjsrbgud@naver.com>
2022-07-04 19:31:39 +0900

    coin tracker

--- ts-react/src/index.tsx
+++ ts-react/src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from 'styled-components';
 import { lightTheme } from "./theme";
-
 const root = ReactDOM.createRoot(document.getElementById('root')as HTMLElement);
 root.render(
   <React.StrictMode>

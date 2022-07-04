6aa8ba5932e27daf78e45eb679ca8d19963f52b4
rnjsrbgud@naver.com <rnjsrbgud@naver.com>
2022-07-04 20:31:21 +0900

    coin tracker

--- ts-react/src/pages/Coins.tsx
+++ ts-react/src/pages/Coins.tsx
@@ -1,8 +1,83 @@
-import React from 'react'
+import React , { useState } from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import  axios  from 'axios';
+
+const Container = styled.section`
+  padding: 0 10px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const CoinList = styled.ul`
+  
+  
+`;
+const Coin = styled.li`
+  background-color: white;
+  color: ${(props)=> props.theme.bgColor};
+  margin-bottom: 10px;
+  border-radius: 15px;
+  a{
+    padding: 20px;
+    transition: color .5s ease-in-out;
+    display: block;
+  }
+  &:hover{
+    a{
+      color: ${props => props.theme.accentColor};
+    }
+  }
+`;
+// 타이틀
+const Title = styled.h1`
+  font-size: 48px;
+  color:${props => props.theme.accentColor};
+`;
+const Loader = styled.div`
+    text-align: center;
+`;
+
+interface CoinInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
 
 const Coins = () => {
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const getCoins = async() =>{
+    const res  =  await axios("https://api.coinpaprika.com/v1/coins");
+    setCoins(res.data.slice(0, 100));
+    setLoading(false);
+  }
+  useEffect(()=>{
+    getCoins();
+  })
   return (
-    <h1>coins</h1>
+    <Container>
+      <Header>
+        <Title>coins</Title>
+      </Header>
+      {loading ? <Loader>"Loading..."</Loader>: <CoinList>
+        {coins.map(coin => <Coin key={coin.id}>
+          <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+        </Coin>)}
+      </CoinList>}
+    </Container>
+
   )
 }
 

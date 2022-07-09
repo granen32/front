e0fb2b7882c42e0996bcfef587533bd08fdbf6db
rnjsrbgud@naver.com <rnjsrbgud@naver.com>
2022-07-09 14:23:20 +0900

    url 링크 변경

--- ts-react/src/api.ts
+++ ts-react/src/api.ts
@@ -1,12 +1,12 @@
 // 이 함수는 json .data를 반환해줘야함 ->fetcher 함수는 promise여야함
 import axios from "axios";
-const COINURL = "https://api.coinpaprika.com/v1/coins";
+const BaseUrl = "https://api.coinpaprika.com/v1/coins";
 
 // 이건 함수형으로 해줘야함
 export const fetchCoins = async() =>{
   // 데이터 얻는법
   // axios.get(url).then(res => console.log(res.data))
-  return await axios.get(`${COINURL}`).then(
+  return await axios.get(`${BaseUrl}`).then(
     (res) =>{
       console.log("Response from hook", res.data);
       return res.data;
